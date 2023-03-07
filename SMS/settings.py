eeda8b0353fec79614ec2fb3f791ab65b0fc018a
Lakshya Chauhan <turboluckyc@gmail.com>
2023-03-07 02:39:58 -0500

    allowed hosts to all

--- SMS/settings.py
+++ SMS/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'o!ld8nrt4vc*h1zoey*wj48x*q0#ss12h=+zh)kk^6b3aygg=!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # change the default user models to our custom model
 AUTH_USER_MODEL = 'accounts.User' 
