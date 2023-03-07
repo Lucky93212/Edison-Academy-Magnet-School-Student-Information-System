ca19a73ab01352b8e810913986652deb2fdaf144
Lakshya Chauhan <turboluckyc@gmail.com>
2023-03-07 02:13:43 -0500

    Debug set to false

--- SMS/settings.py
+++ SMS/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o!ld8nrt4vc*h1zoey*wj48x*q0#ss12h=+zh)kk^6b3aygg=!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
